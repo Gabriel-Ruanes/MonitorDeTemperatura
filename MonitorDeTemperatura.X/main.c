6310e0923bd1d3f66fa1b671aafd2edf8c17b7a6
Gabriel Ruanes Melquiades <78973846+Gabriel-Ruanes@users.noreply.github.com>
2021-09-23 14:30:36 -0300

    Add files

---
+++ MonitorDeTemperatura.X/main.c
@@ -0,0 +1,16 @@
+/*
+ * File:   main.c
+ * Author: 21192694
+ *
+ * Created on 23 de Setembro de 2021, 13:29
+ */
+
+#include "config.h"
+#include <xc.h>
+
+void main(void) 
+{
+    
+    
+    return;
+}
